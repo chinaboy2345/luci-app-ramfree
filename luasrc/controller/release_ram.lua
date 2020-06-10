a9acac49a0c7828eb786fc46080be39c88cd715c
stuarthua <stuarthua.cn@gmail.com>
2020-06-10 16:41:25 +0800

    repost by shell.

--- luasrc/controller/release_ram.lua
+++ luasrc/controller/release_ram.lua
@@ -1,8 +1,7 @@
 module("luci.controller.release_ram",package.seeall)
 
 function index()
-    entry({"admin", "custom"}, firstchild(), "我的", 89).dependent = false
-    entry({"admin","custom","release_ram"}, call("release_ram"), _("释放内存"), 9999)
+    entry({"admin", "services", "release_ram"}, call("release_ram"), _("释放内存"), 9999).dependent = true
 end
 function release_ram()
     luci.sys.call("sync && echo 3 > /proc/sys/vm/drop_caches")
